3f131788d1e2e0d79cb54d33156ed730bbd67670
Ahmed Abushagur <52082882+AhmedTMM@users.noreply.github.com>
2025-07-21 17:50:50 -0700

    feat: initial changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,47 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import logo from "../data/LiftsPump.png"
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <header className={styles.header}>
+        <Image src={logo} alt="LiftsPump logo" className={styles.logo}/>
+        <div className={styles.titleText} >
+          LIFTSPUMP
         </div>
+        <Avatar sx={{ bgcolor: 'white', color: '#1AE080' }} className={styles.profileImage}>AA</Avatar>
+      </header>
+      <main className={styles.main}>
+        <List className={styles.listSelector}>
+          <ListItemButton>
+            <ListItemIcon>
+               <Avatar sx={{ bgcolor: 'white', color: '#1AE080' }} className={styles.profileImage}>R</Avatar>
+            </ListItemIcon>
+            <ListItemText primary="Routines"/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+               <Avatar sx={{ bgcolor: 'white', color: '#1AE080' }} className={styles.profileImage}>U</Avatar>
+            </ListItemIcon>
+            <ListItemText primary="Users"/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+               <Avatar sx={{ bgcolor: 'white', color: '#1AE080' }} className={styles.profileImage}>P</Avatar>
+            </ListItemIcon>
+            <ListItemText primary="Payments"/>
+          </ListItemButton>
+        </List>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
