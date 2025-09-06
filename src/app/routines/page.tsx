3e3cbc45ac81c9940e631db35e3b478b621238bc
Ahmed Abushagur <ahmed@abushagur.com>
2025-09-06 12:52:29 -0700

    feat: routine ground work

--- src/app/routines/page.tsx
+++ src/app/routines/page.tsx
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import styles from "../page.module.css";
 import Navigation from "../../components/Navigation";
 import { Box, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import Divider from '@mui/material/Divider';
 
 enum RoutineItem {
   Cardio = "Cardio",
@@ -19,9 +20,22 @@ export default function Routines() {
           <h1>Routines</h1>
           <Box
           sx={{
+            display: "flex",
             maxHeight: "100vh",
-            overflowY: "auto"
+            overflowY: "auto",
+            flexDirection: "row"
           }}>
+            <List>
+              {Object.values(RoutineItem).map((item) => (
+                <ListItemButton key={item}>
+                  <ListItemIcon>
+                    {/*<img src={`/icons/${item.toLowerCase()}.png`} alt={item} width={24} height={24} />*/}
+                  </ListItemIcon>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              ))}
+            </List>
+            <Divider orientation="vertical"/>
             <List>
               {Object.values(RoutineItem).map((item) => (
                 <ListItemButton key={item}>
